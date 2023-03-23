52dbe08d7579221024e30d39f2bfcc57e7bd29ac
Ubax <ubax7k@gmail.com>
2023-03-23 23:23:29 +0100

    feat: Updates README

--- src/Calculator/constants.js
+++ src/Calculator/constants.js
@@ -1,14 +1,46 @@
 // TODO[i18n]
 
 export const INITIAL_ITEMS = [
-    { name: "Chair", stickers: 1, img: "/img/chair.svg" },
-    { name: "Small sofa", stickers: 1, img: "/img/small-sofa.svg" },
-    { name: "Big sofa", stickers: 2, img: "/img/big-sofa.svg" },
-    { name: "Cupboard", stickers: 1, img: "/img/cupboard.svg" },
-    { name: "Wardrobe", stickers: 2, img: "/img/wardrobe.svg" },
-    { name: "Bag max 120l", stickers: 1, img: "/img/trash.png" },
-    { name: "Single mattress", stickers: 1, img: "/img/small-mattress.png" },
-    { name: "Double mattress", stickers: 2, img: "/img/big-mattress.png" },
-  ];
-  
+  {
+    name: "Chair",
+    stickers: 1,
+    img: `${process.env.PUBLIC_URL}/img/chair.svg`,
+  },
+  {
+    name: "Small sofa",
+    stickers: 1,
+    img: `${process.env.PUBLIC_URL}/img/small-sofa.svg`,
+  },
+  {
+    name: "Big sofa",
+    stickers: 2,
+    img: `${process.env.PUBLIC_URL}/img/big-sofa.svg`,
+  },
+  {
+    name: "Cupboard",
+    stickers: 1,
+    img: `${process.env.PUBLIC_URL}/img/cupboard.svg`,
+  },
+  {
+    name: "Wardrobe",
+    stickers: 2,
+    img: `${process.env.PUBLIC_URL}/img/wardrobe.svg`,
+  },
+  {
+    name: "Bag max 120l",
+    stickers: 1,
+    img: `${process.env.PUBLIC_URL}/img/trash.png`,
+  },
+  {
+    name: "Single mattress",
+    stickers: 1,
+    img: `${process.env.PUBLIC_URL}/img/small-mattress.png`,
+  },
+  {
+    name: "Double mattress",
+    stickers: 2,
+    img: `${process.env.PUBLIC_URL}/img/big-mattress.png`,
+  },
+];
+
 export const STAMP_PRICE = 8.6;
