e6c7698adf3ef58a22b57e9cfbcd95b2255820b0
Ubax <ubax7k@gmail.com>
2023-03-23 20:09:31 +0200

    refactor: Moves images to img

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -5,14 +5,14 @@ import { Item } from "./Item";
 import { SummaryItem } from "./SummaryItem";
 
 const INITIAL_ITEMS = [
-  { name: "Chair", stickers: 1, img: "/chair.svg" },
-  { name: "Small sofa", stickers: 1, img: "/small-sofa.svg" },
-  { name: "Big sofa", stickers: 2, img: "/big-sofa.svg" },
-  { name: "Cupboard", stickers: 1, img: "/cupboard.svg" },
-  { name: "Wardrobe", stickers: 2, img: "/wardrobe.svg" },
-  { name: "Bag max 120l", stickers: 1, img: "/trash.png" },
-  { name: "Single mattress", stickers: 1, img: "/small-mattress.png" },
-  { name: "Double mattress", stickers: 2, img: "/big-mattress.png" },
+  { name: "Chair", stickers: 1, img: "/img/chair.svg" },
+  { name: "Small sofa", stickers: 1, img: "/img/small-sofa.svg" },
+  { name: "Big sofa", stickers: 2, img: "/img/big-sofa.svg" },
+  { name: "Cupboard", stickers: 1, img: "/img/cupboard.svg" },
+  { name: "Wardrobe", stickers: 2, img: "/img/wardrobe.svg" },
+  { name: "Bag max 120l", stickers: 1, img: "/img/trash.png" },
+  { name: "Single mattress", stickers: 1, img: "/img/small-mattress.png" },
+  { name: "Double mattress", stickers: 2, img: "/img/big-mattress.png" },
 ];
 
 export function Calculator(props) {
