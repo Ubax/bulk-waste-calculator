9d2acd0b2772988215666605b053b93cde165715
Ubax <ubax7k@gmail.com>
2023-03-24 06:32:41 +0100

    feat: Adds custom palette

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import { extendTheme, CssVarsProvider } from "@mui/joy";
 import { Calculator } from "./Calculator/Calculator";
 
+const customTheme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: {
+          900: "#c5000e",
+          800: "#d3001e",
+          700: "#e00025",
+          600: "#e00025",
+          500: "#e00025",
+          400: "#fb4349",
+          300: "#f06a6e",
+          200: "#f79597",
+          100: "#ffcbd1",
+          50: "#ffeaee",
+        },
+        focusVisible: "rgba(3, 102, 214, 0.3)",
+      },
+    },
+  },
+});
+
+//E00025
+
 function App() {
-  return <Calculator />;
+  return (
+    <CssVarsProvider theme={customTheme}>
+      <Calculator />
+    </CssVarsProvider>
+  );
 }
 
 export default App;
